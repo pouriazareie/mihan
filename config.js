71f7f7b6e896ba973161710108ce6142054cecd2
pouriazareie <pouria.zareie@gmail.com>
2020-02-19 07:52:58 -0800

    Create  config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+// The following list of parameters are supported to be added after 123flashchat.html
+// init configuration
+var init_host;
+var init_port;
+var init_host_s;
+var init_port_s;
+var init_host_h;
+var init_port_h;
+var init_group;
+var init_room;
+var init_room_pwd;
+var init_listroom;
+var init_loading_bk;
+var init_skin;
+var init_lang;
+var init_user;
+var init_password;
+var init_nickname;
+var init_location;
+var init_gender;
+var init_birth;
+//var init_extensions = "seat,screenmsg,games,youtube,fan,like,twitter";____________(alirezaaaaaaaaaaa)
+// End of init configuration
+
+var enable_google_analytics = true;
+var camshow_offline_page = "offline/html/a.html";
