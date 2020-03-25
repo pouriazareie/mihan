62a3a7e7142e6d075c252fe595aa105c9c3fde7d
pouriazareie <pouria.zareie@gmail.com>
2020-03-25 12:28:19 -0700

    Create ALIREZA.js

---
+++ ALIREZA.js
@@ -0,0 +1,121 @@
+
+for (var i=0; i<30; i++) (function(t) {
+  window.setTimeout(function() {
+
+ var existCondition6 = setInterval(function() {
+ if ($('#topcmm-123flashchat-main-message-input').length) {
+    console.log("existCondition6 Exists!!");
+	document.getElementById("topcmm-123flashchat-main-message-input").value=Math.random().toString(24).substr(2, 5);
+    clearInterval(existCondition6);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+//existCondition7
+var existCondition7 = setInterval(function() {
+ if ($('#topcmm-123flashchat-main-send-msg-btn').length) {
+    console.log("existCondition7 Exists!!");
+	document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
+    clearInterval(existCondition7);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+
+
+    
+ }, t*100)
+}(i)) 
+
+//existCondition1
+var existCondition1 = setInterval(function() {
+ if ($('#topcmm-123flashchat-mainview-top-settings-btn').length) {
+    console.log("existCondition1 Exists!");
+    clearInterval(existCondition1);
+	document.getElementById("topcmm-123flashchat-mainview-top-settings-btn").click();
+ }
+}, 100); // check every 5000ms
+//existCondition2
+var existCondition2 = setInterval(function() {
+ if ($('#topcmm-123flashchat-settings-menuitem-signin').length) {
+    console.log("existCondition2 Exists!!");
+    clearInterval(existCondition2);
+document.getElementById("topcmm-123flashchat-settings-menuitem-signin").click();
+ }
+}, 100); // check every 5000ms
+//existCondition3
+var existCondition3 = setInterval(function() {
+ if ($('#topcmm-123flashchat-loginview-username-input').length) {
+    console.log("existCondition3 Exists!!");
+    clearInterval(existCondition3);
+		document.getElementById("topcmm-123flashchat-sign-in-guest-checkbox-block").style="display: show;";
+ }
+}, 100); // check every 100ms
+//existCondition31
+var existCondition3as1 = setInterval(function() {
+ if ($('#topcmm-123flashchat-loginview-guest-checkbox').length) {
+    console.log("existCondition3as1 Exists!!");
+    clearInterval(existCondition3as1);
+	document.getElementById("topcmm-123flashchat-loginview-guest-checkbox").click();
+ }
+}, 100); // check every 100ms
+//existCondition4
+var existCondition4 = setInterval(function() {
+ if ($('#topcmm-123flashchat-loginview-username-input').length) {
+    console.log("existCondition4 Exists!!");
+    clearInterval(existCondition4);
+	document.getElementById("topcmm-123flashchat-loginview-username-input").value=Math.random().toString(36).substr(2, 6);
+ }
+}, 100); // check every 5000ms
+//OK
+//existCondition5
+var existCondition5 = setInterval(function() {
+ if ($('#topcmm-123flashchat-loginview-login-btn').length) {
+    console.log("existCondition5 Exists!!");
+document.getElementById("topcmm-123flashchat-loginview-login-btn").click();
+    clearInterval(existCondition5);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+//existCondition6
+var existCondition6 = setInterval(function() {
+ if ($('#topcmm-123flashchat-main-message-input').length) {
+    console.log("existCondition6 Exists!!");
+	document.getElementById("topcmm-123flashchat-main-message-input").value=Math.random().toString(24).substr(2, 5);
+    clearInterval(existCondition6);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+//existCondition7
+var existCondition7 = setInterval(function() {
+ if ($('#topcmm-123flashchat-main-send-msg-btn').length) {
+    console.log("existCondition7 Exists!!");
+	document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
+    clearInterval(existCondition7);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+
+for (var i=0; i<30; i++) (function(t) {
+  window.setTimeout(function() {
+	  var existCondition6 = setInterval(function() {
+ if ($('#topcmm-123flashchat-main-message-input').length) {
+    console.log("existCondition6 Exists!!");
+	document.getElementById("topcmm-123flashchat-main-message-input").value='*(&(&^&*&%^&$##@#$@#$@(*_';
+	document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
+    clearInterval(existCondition6);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+//existCondition7
+var existCondition7 = setInterval(function() {
+ if ($('#topcmm-123flashchat-main-send-msg-btn').length) {
+    console.log("existCondition7 Exists!!");
+	document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
+    clearInterval(existCondition7);
+    doTheRestOfTheStuff(parameters);
+ }
+}, 100); // check every 100ms
+    for (var i=0; i<30; i++) (function(t) {
+  window.setTimeout(function() {
+
+ }, t*100)
+}(i)) 
