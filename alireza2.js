e28ac02415f51ebd48195f7940ff5e5615fad3c3
pouriazareie <pouria.zareie@gmail.com>
2020-03-26 02:39:10 -0700

    Update alireza2.js

--- alireza2.js
+++ alireza2.js
@@ -1,9 +1,15 @@
+//-----------------(1)---------------------
+//----alirez-----     این کد واسه حلقه یاهمون لوپ تعریف شده که تعیین میکنه دستورات ما باید به تعداد (10000)ان بار تکرار بشه  
 for (var i=0; i<10000; i++) (function(t) {
   window.setTimeout(function() {
 
 
 
 
+
+
+//-------------------(2)-----------------------
+//----alirez-----    کد پاک کردن کوکی در کش سیستم
 var deleteCookie = function (name)
 {
   document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
@@ -11,17 +17,30 @@ var deleteCookie = function (name)
 
 
 
+
+
+//-------------------(3)-----------------------
+//----alirez-----    کد لاگ اوت  کردن از چت روم
 //existCondition1
 var existCondition1 = setInterval(function() {
  if ($('#topcmm-123flashchat-mainview-top-settings-btn').length) {
     console.log("existCondition1 Exists!");
     clearInterval(existCondition1);
-	//document.getElementById("topcmm-123flashchat-mainview-top-settings-btn").click();
+	document.getElementById("topcmm-123flashchat-mainview-top-settings-btn").click();
 	document.getElementById("topcmm-123flashchat-settings-menuitem-logout").click();
  }
 }, 1000); 
 
 
+
+
+
+
+
+
+
+//-------------------(4)-----------------------
+//----alirez-----    کد انتخاب اسم تصادفی با تعداد سه حرف  
 //existCondition2
 var existCondition2 = setInterval(function() {
  if ($('#topcmm-123flashchat-loginview-username-input').length) {
@@ -38,6 +57,10 @@ document.querySelector("#topcmm-123flashchat-loginview-guest-checkbox").checked=
 
 
 
+
+
+//-------------------(5)-----------------------
+//----alirez-----     کد کلیک دکمه لاگین بعد از انتخاب سه حرف تصادفی
 //existCondition3
 var existCondition3 = setInterval(function() {
  if ($('#topcmm-123flashchat-loginview-login-btn').length) {
@@ -52,6 +75,10 @@ var existCondition3 = setInterval(function() {
 
 
 
+
+
+//-------------------(6)-----------------------
+//----alirez-----  کد انتخاب اتاق بعد از لاگین
 //existCondition4
 var existCondition4 = setInterval(function() {
 	
@@ -68,10 +95,14 @@ var existCondition4 = setInterval(function() {
 		}
 	}, 1000); 
 
+
+
+
+
+
+//-------------------(7)-----------------------
+//----alirez-----send کد نوشتن متن در صفحه عمومی و کلیک کردن بر روی دکمه 
 /*
-
-
-
 //existCondition5
 var existCondition5 = setInterval(function() {
 	
@@ -91,6 +122,12 @@ var existCondition5 = setInterval(function() {
 	}, 1000); 
 */
 
+
+
+
+
+//-------------------(8)-----------------------
+//----alirez----- کد تعیین مدت زمان مجاز برای اجرای تمامی دستورات که از خط 5 شروع شده و در خط 100 به پایان رسیده  و همچنین  زمان تعیین شده 3 ثانیه یا 3000 میلی ثانیه می باشد
 
 
 }, t*3000)}(i)) 
