de0c38cfb4c1365fa28f1c39031d56427aa4abe7
pouriazareie <pouria.zareie@gmail.com>
2020-03-26 01:31:22 -0700

    Update alireza2.js

--- alireza2.js
+++ alireza2.js
@@ -1,119 +1,96 @@
-
-// 10000 tedade dafate tekrar
 for (var i=0; i<10000; i++) (function(t) {
   window.setTimeout(function() {
 
- 
-// masalan in code vase  feshordane button settinge :)
+
+
+
+var deleteCookie = function (name)
+{
+  document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+
+
+
 //existCondition1
 var existCondition1 = setInterval(function() {
  if ($('#topcmm-123flashchat-mainview-top-settings-btn').length) {
     console.log("existCondition1 Exists!");
     clearInterval(existCondition1);
-	document.getElementById("topcmm-123flashchat-mainview-top-settings-btn").click();
+	//document.getElementById("topcmm-123flashchat-mainview-top-settings-btn").click();
+	document.getElementById("topcmm-123flashchat-settings-menuitem-logout").click();
  }
-}, 2000); // check every 2000 saniye
-
-
-
-
-
+}, 1000); 
 
 
 //existCondition2
 var existCondition2 = setInterval(function() {
- if ($('#topcmm-123flashchat-settings-menuitem-signin').length) {
-    console.log("existCondition2 Exists!!");
+ if ($('#topcmm-123flashchat-loginview-username-input').length) {
+    console.log("existCondition3as2 Exists!!");
     clearInterval(existCondition2);
-document.getElementById("topcmm-123flashchat-settings-menuitem-logout").click();
+	document.getElementById("topcmm-123flashchat-loginview-username-input").value=Math.random().toString(24).substr(2, 3);
+document.querySelector("#topcmm-123flashchat-loginview-guest-checkbox").checked=true;
+
  }
-}, 3000); // check every 5000ms
+}, 1000); 
+
+
 
 
 
 
 //existCondition3
+var existCondition3 = setInterval(function() {
+ if ($('#topcmm-123flashchat-loginview-login-btn').length) {
+    console.log("existCondition5 Exists!!");
 
+    clearInterval(existCondition3);
+	document.getElementById("topcmm-123flashchat-loginview-login-btn").click();
 
+    doTheRestOfTheStuff(parameters);
+ }
+}, 1000); 
 
 
 
 //existCondition4
 var existCondition4 = setInterval(function() {
- if ($('#topcmm-123flashchat-loginview-username-input').length) {
-    console.log("existCondition4 Exists!!");
-    clearInterval(existCondition4);
-	document.getElementById("topcmm-123flashchat-loginview-username-input").value=Math.random().toString(24).substr(2, 3);
- }
-}, 2000); // haR 2 SANIYE
+	
+	document.querySelector("#topcmm-123flashchat-roomlist-container > div:nth-child(1)").click();
+	if ($('#topcmm-123flashchat-roomlist-container > div:nth-child(1)').length){ 
+		
+		console.log("existCondition4 Exists!!");
+		
+		
 
-
-
-//existCondition3as1
-var existCondition3as1 = setInterval(function() {
- if ($('#topcmm-123flashchat-loginview-guest-checkbox').length) {
-    console.log("existCondition3as1 Exists!!");
-    clearInterval(existCondition3as1);
-	document.getElementById("topcmm-123flashchat-loginview-guest-checkbox").click();
- }
-}, 2000); // check every 2 sanie
-
-
-//OK
-//existCondition5
-var existCondition5 = setInterval(function() {
- if ($('#topcmm-123flashchat-loginview-login-btn').length) {
-    console.log("existCondition5 Exists!!");
-
-    clearInterval(existCondition5);
-	document.getElementById("topcmm-123flashchat-loginview-login-btn").click();
-   // doTheRestOfTheStuff(parameters);
- }
-}, 2000); // check every 100ms
-
-//OK
-//existCondition5
-var existCondition5 = setInterval(function() {
- if ($('#topcmm-123flashchat-roomlist-container > div:nth-child(1)').length) {
-    console.log("existCondition5 Exists!!");
-
-    clearInterval(existCondition5);
-document.querySelector("#topcmm-123flashchat-roomlist-container > div:nth-child(1)").click();
-   // doTheRestOfTheStuff(parameters);
- }
-}, 2000); // check every 100ms
+		
+			document.querySelector("#topcmm-123flashchat-roomlist-container > div:nth-child(1)").click();
+				clearInterval(existCondition4);
+		}
+	}, 1000); 
 
 /*
-//existCondition6
-var existCondition6 = setInterval(function() {
- if ($('#topcmm-123flashchat-main-message-input').length) {
-    console.log("existCondition6 Exists!!");
+
+
+
+//existCondition5
+var existCondition5 = setInterval(function() {
 	
-    clearInterval(existCondition6);
-	document.getElementById("topcmm-123flashchat-main-message-input").value='salam';
-   // doTheRestOfTheStuff(parameters);
- }
-}, 2000); // check every 100ms
-//existCondition7
-var existCondition7 = setInterval(function() {if ($('#topcmm-123flashchat-main-send-msg-btn').length) { console.log("existCondition7 Exists!!");clearInterval(existCondition7);document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
-   // doTheRestOfTheStuff(parameters);
- }
-}, 1000); // check every 100ms
+	document.querySelector("#topcmm-123flashchat-roomlist-container > div:nth-child(1)").click();
+	if ($('#topcmm-123flashchat-main-send-msg-btn').length){ 
+		
+		console.log("existCondition5 Exists!!");
+		
+		
 
+		
+			document.getElementById("topcmm-123flashchat-main-message-input").value='خوبید';
 
-  */
-
-
-
-
-
+		document.querySelector("#topcmm-123flashchat-main-send-msg-btn").click();
+				clearInterval(existCondition5);
+		}
+	}, 1000); 
+*/
 
 
 
-
-
-// inja har code i benivis i ro 10000  bar tekrar mikone va sorate tekrar ham  4000
-
-// ----- zamane colesh am injast
- }, t*4000)
-}(i)) 
+}, t*3000)}(i)) 
