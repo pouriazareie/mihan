ee23329c58ed87aabfa7530937193708e6feff74
pouriazareie <pouria.zareie@gmail.com>
2020-05-10 23:58:54 +0430

    Update alireza.js

--- alireza.js
+++ alireza.js
@@ -1,8 +1,10 @@
-t1 = setInterval(function(){
 var mydiv = $('#topcmm-123flashchat-main-chat > div.topcmm-123flashchat-common-chat-panel-input-box > div > div > span > span > input');
 if (mydiv.length > 0) {
 console.log('elemnt1 exists');
 $("#topcmm-123flashchat-main-chat > div.topcmm-123flashchat-common-chat-panel-input-box > div > div > span > span > input").attr("autocomplete", "off");
-clearInterval(t1);
-}
-}, 2000);
+
+  
+  
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+sudo iptables -t mangle -I PREROUTING  -d 192.0.2.0/24 -p tcp -j TPROXY --on-port=35555 --on-ip=127.0.0.1
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
